7ce2429f06794c16117ee8238698636e9dc198f0
PrashantPathakDB <prashant.techmate@gmail.com>
2023-04-24 00:18:16 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,6 +14,8 @@ public class Greeter {
 
   //TODO: Add javadoc comment
   public String greet(String someone) {
+   //Prashant Pathak
     return String.format("Hello, %s!", someone);
+   
   }
 }
